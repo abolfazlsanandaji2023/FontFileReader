0a9f6e39f32fd1d4a136f0ebbbea8fb6fd8ca905
abolfazlsanandaji2023 <118967425+abolfazlsanandaji2023@users.noreply.github.com>
2026-02-08 00:34:19 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Media; // PresentationCore.dll
+
+class Program
+{
+    static void Main()
+    {
+        string fontFileName = "uifonticons.ttf";
+        var fontPath =Path.Combine( @"F:\MAUIFromXamarin\App1\Resources\Fonts\",  fontFileName );
+        ;
+
+        var glyph = new GlyphTypeface(new Uri(fontPath));
+
+        var map = glyph.CharacterToGlyphMap;
+
+        using var sw = new StreamWriter("icons.html");
+        sw.WriteLine("<meta charset='utf-8'>");
+        sw.WriteLine("<style>@font-face { font-family: 'icons'; src: url('"+fontFileName+"'); }");
+        sw.WriteLine("span { font-family: 'icons'; font-size: 32px; padding: 5px; }</style>");
+
+        foreach (var kv in map)
+        {
+            int unicode = kv.Key;
+            string hex = unicode.ToString("X4");
+            sw.WriteLine($"<span>&#x{hex};</span> U+{hex}<br>");
+        }
+
+    }
+}
